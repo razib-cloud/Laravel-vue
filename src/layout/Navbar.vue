88e0751363a846f507d7b8e65de59eb83fea99f7
razib-cloud <razibidb61@gmail.com>
2025-03-25 12:10:17 +0600

    api

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -79,18 +79,8 @@
   </div>
 </template>
 
-<script lang="ts" setup>
-
-
-
-
-
-
-
-</script>
+<script lang="ts" setup></script>
 
 <style>
-
-
 
 </style>
--- src/layout/SideBar.vue
+++ src/layout/SideBar.vue
@@ -14,60 +14,9 @@
           <ul aria-expanded="false">
             <li><a href="index.html">Dashboard Light</a></li>
             <li><a href="index-2.html">Dashboard Dark</a></li>
-            <li><a href="order-page-list.html">Orders</a></li>
-            <li><a href="order-details-page.html">Order Details</a></li>
-            <li><a href="customer-page-list.html">Customers</a></li>
-            <li><a href="analytics.html">Analytics</a></li>
-            <li><a href="review.html">Review</a></li>
           </ul>
         </li>
-        <li>
-          <a
-            class="has-arrow ai-icon"
-            href="javascript:void()"
-            aria-expanded="false"
-          >
-            <i class="flaticon-050-info"></i>
-            <span class="nav-text">Apps</span>
-          </a>
-          <ul aria-expanded="false">
-            <li><a href="app-profile.html">Profile</a></li>
-            <li><a href="post-details.html">Post Details</a></li>
-            <li>
-              <a
-                class="has-arrow"
-                href="javascript:void()"
-                aria-expanded="false"
-                >Email</a
-              >
-              <ul aria-expanded="false">
-                <li><a href="email-compose.html">Compose</a></li>
-                <li><a href="email-inbox.html">Inbox</a></li>
-                <li><a href="email-read.html">Read</a></li>
-              </ul>
-            </li>
-            <li><a href="app-calender.html">Calendar</a></li>
-            <li>
-              <a
-                class="has-arrow"
-                href="javascript:void()"
-                aria-expanded="false"
-                >Shop</a
-              >
-              <ul aria-expanded="false">
-                <li><a href="ecom-product-grid.html">Product Grid</a></li>
-                <li><a href="ecom-product-list.html">Product List</a></li>
-                <li>
-                  <a href="ecom-product-detail.html">Product Details</a>
-                </li>
-                <li><a href="ecom-product-order.html">Order</a></li>
-                <li><a href="ecom-checkout.html">Checkout</a></li>
-                <li><a href="ecom-invoice.html">Invoice</a></li>
-                <li><a href="ecom-customers.html">Customers</a></li>
-              </ul>
-            </li>
-          </ul>
-        </li>
+        
         <li>
           <a
             class="has-arrow ai-icon"
@@ -81,64 +30,13 @@
             <li><a href="chart-flot.html">Flot</a></li>
             <li><a href="chart-morris.html">Morris</a></li>
             <li><a href="chart-chartjs.html">Chartjs</a></li>
-            <li><a href="chart-chartist.html">Chartist</a></li>
-            <li><a href="chart-sparkline.html">Sparkline</a></li>
-            <li><a href="chart-peity.html">Peity</a></li>
           </ul>
         </li>
-        <li>
-          <a
-            class="has-arrow ai-icon"
-            href="javascript:void()"
-            aria-expanded="false"
-          >
-            <i class="flaticon-086-star"></i>
-            <span class="nav-text">Bootstrap</span>
-          </a>
-          <ul aria-expanded="false">
-            <li><a href="ui-accordion.html">Accordion</a></li>
-            <li><a href="ui-alert.html">Alert</a></li>
-            <li><a href="ui-badge.html">Badge</a></li>
-            <li><a href="ui-button.html">Button</a></li>
-            <li><a href="ui-modal.html">Modal</a></li>
-            <li><a href="ui-button-group.html">Button Group</a></li>
-            <li><a href="ui-list-group.html">List Group</a></li>
-            <li><a href="ui-card.html">Cards</a></li>
-            <li><a href="ui-carousel.html">Carousel</a></li>
-            <li><a href="ui-dropdown.html">Dropdown</a></li>
-            <li><a href="ui-popover.html">Popover</a></li>
-            <li><a href="ui-progressbar.html">Progressbar</a></li>
-            <li><a href="ui-tab.html">Tab</a></li>
-            <li><a href="ui-typography.html">Typography</a></li>
-            <li><a href="ui-pagination.html">Pagination</a></li>
-            <li><a href="ui-grid.html">Grid</a></li>
-          </ul>
-        </li>
-        <li>
-          <a
-            class="has-arrow ai-icon"
-            href="javascript:void()"
-            aria-expanded="false"
-          >
-            <i class="flaticon-045-heart"></i>
-            <span class="nav-text">Plugins</span>
-          </a>
-          <ul aria-expanded="false">
-            <li><a href="uc-select2.html">Select 2</a></li>
-            <li><a href="uc-nestable.html">Nestedable</a></li>
-            <li><a href="uc-noui-slider.html">Noui Slider</a></li>
-            <li><a href="uc-sweetalert.html">Sweet Alert</a></li>
-            <li><a href="uc-toastr.html">Toastr</a></li>
-            <li><a href="map-jqvmap.html">Jqv Map</a></li>
-            <li><a href="uc-lightgallery.html">Light Gallery</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="widget-basic.html" class="ai-icon" aria-expanded="false">
-            <i class="flaticon-013-checkmark"></i>
-            <span class="nav-text">Widget</span>
-          </a>
-        </li>
+        
+       
+       
+         
+      
         <li>
           <a
             class="has-arrow ai-icon"
@@ -152,8 +50,6 @@
             <li><a href="form-element.html">Form Elements</a></li>
             <li><a href="form-wizard.html">Wizard</a></li>
             <li><a href="form-ckeditor.html">CkEditor</a></li>
-            <li><a href="form-pickers.html">Pickers</a></li>
-            <li><a href="form-validation.html">Form Validate</a></li>
           </ul>
         </li>
         <li>
@@ -216,5 +112,9 @@
 <script lang="ts" setup>
 
 </script>
+<style>
 
-<style></style>
+
+
+</style>
+
